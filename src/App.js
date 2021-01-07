634f4936d4fd66d5f7fb7441966ac30a30be923c
Damola Olusakin <olusakinadedamola@gmail.com>
2021-01-07 18:21:22 +0100

    add state for country

--- src/App.js
+++ src/App.js
@@ -1,23 +1,34 @@
-
+import { useState } from "react";
 import './App.css';
 import { MenuItem, FormControl, Select } from "@material-ui/core";
 
 function App() {
+  const [countries, setCountries] = useState(["USA", "UK", "INDIA"]);
+
   return (
     <div className="app">
+      <div className="app__header">
       <h1>Covid 19 Tracker</h1>
       <FormControl className="app__dropdown">
         <Select
           variant="outlined"
           value="abc"
         >
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Worldwide</MenuItem>
-          <MenuItem value="worldwide">Worldwide</MenuItem>
+              {
+                countries.map((country) => (
+                  <MenuItem value={country}>{country}</MenuItem>
+                ))
+              }
+
+          {/*<MenuItem value="worldwide">Worldwide</MenuItem>
+          <MenuItem value="worldwide">Option1</MenuItem>
+          <MenuItem value="worldwide">Option2</MenuItem>
+  <MenuItem value="worldwide">Greatstuff</MenuItem>*/}
 
         </Select>
       </FormControl>
+      </div>
+      
 
         {/*Header*/}
         {/*Title + Select inout dropdown field*/}
